e6465d25163a04520e24742db7367f71aec8f7c6
venice1200 <43762187+venice1200@users.noreply.github.com>
2019-02-28 18:48:21 +0100

    Add files via upload

--- examples/EdgeTest/EdgeTest.ino
+++ examples/EdgeTest/EdgeTest.ino
@@ -1,8 +1,9 @@
 /*
-  Simple Esge Test Sketch
+  Simple Edge Test Sketch
   Venice1200 02/2019
 */
 
+// Include Edge Header File
 #include "Edge.h"
 
 #define Button2 11
@@ -10,7 +11,7 @@
 #define LEDL 13
 #define LEDR 6
 
-
+// Define Edge Objects
 EDGE b1edge;
 EDGE b2edge;
 
@@ -23,18 +24,23 @@ void setup() {
   pinMode(Button2, INPUT_PULLUP);
   pinMode(Button3, INPUT_PULLUP);
 
+  // Init Edge Objects
   b1edge.init();
   b2edge.init();
 }
 
 void loop() {
+  // You have to update all Edge Objects each cycle 
+  // with your boolean Input Signal
   b1edge.update(digitalRead(Button2));
   b2edge.update(digitalRead(Button3));
-
+  
+  // Detect Signal changes for Input Button 2
   if (b1edge.rising())  Serial.println("B2 Rising Signal 0->1 detected !");
   if (b1edge.falling()) Serial.println("B2 Falling Signal 1->0 detected !");
   if (b1edge.changed()) Serial.println("B2 Signal change detected !");
-  
+
+  // Detect Signal changes for Input Button 3
   if (b2edge.rising())  Serial.println("B3 Rising Signal 0->1 detected !");
   if (b2edge.falling()) Serial.println("B3 Falling Signal 1->0 detected !");
   if (b2edge.changed()) Serial.println("B3 Signal change detected !");
